1b029d1efe876d300b002315faf9ff6dd5891d81
jmeier07 <jmeier07@gmail.com>
2019-08-04 19:30:07 -0400

    committing stuff

---
+++ app.js
@@ -0,0 +1,17 @@
+// Express.JS server dependency
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin'); 
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+app.listen(3000);
