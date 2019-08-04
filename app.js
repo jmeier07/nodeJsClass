01cc8e2a24f6f37f20a25f17e81049c3dbac4c4c
jmeier07 <jmeier07@gmail.com>
2019-08-04 19:54:39 -0400

    lecture 71

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Express.JS server dependency
 const express = require('express');
 
+const path = require('path');
+
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin'); 
@@ -10,8 +12,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname,'views','404.html'));
+});
 
 
 app.listen(3000);
