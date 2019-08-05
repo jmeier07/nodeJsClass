07edea36ce082420320985f4d29cc38c44166918
jmeier07 <jmeier07@gmail.com>
2019-08-04 20:40:46 -0400

    through lecture 77

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-// Express.JS server dependency
-const express = require('express');
-
 const path = require('path');
 
+const express = require('express');
 const bodyParser = require('body-parser');
-
-const adminRoutes = require('./routes/admin'); 
-const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.sendFile(path.join(__dirname,'views','404.html'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,23 @@
-const express = require('express');
-
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product', (req,res,next) => {
-    res.sendFile(path.join( rootDir,'views','add-product.html'));
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/add-product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = router;
-
+exports.routes = router;
+exports.products = products;
